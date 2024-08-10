54568ba7a87278db7d738be111260a42b10234b0
Kanishk <kanishkteotia5077@gmail.com>
2024-08-10 15:33:48 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,27 @@
+import { useState } from 'react';
+
 const Home = () => {
-  return <div>Home Page</div>;
+  const [searchStr, setStrValue] = useState('');
+
+  const onSearchInputChange = ev => {
+    setStrValue(ev.target.value);
+  };
+
+  const onSearch = async ev => {
+    ev.preventDefault();
+    const  response = await fetch('https://ghibliapi.herokuapp.com/films')
+    const body = await response.json()
+      .then(response => response.json())
+      .then(body => console.log(body));
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchInputChange} />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 };
 export default Home;
