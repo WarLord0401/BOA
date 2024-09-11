b237733aaca5957c466a37d4130f8c2c6fea5419
Kanishk <kanishkteotia5077@gmail.com>
2024-09-11 20:41:21 +0530

    hoya added api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,47 @@
 import { useState } from 'react';
+import { searchForShows } from '../api/tvmaze';
 
 const Home = () => {
-  const [searchStr, setStrValue] = useState('');
-
+  const [searchStr, setSearchStr] = useState('');
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiErrorData] = useState(null);
   const onSearchInputChange = ev => {
-    setStrValue(ev.target.value);
+    setSearchStr(ev.target.value);
   };
 
   const onSearch = async ev => {
     ev.preventDefault();
-    const  response = await fetch('https://ghibliapi.herokuapp.com/films')
-    const body = await response.json()
-      .then(response => response.json())
-      .then(body => console.log(body));
+    try {
+      setApiErrorData(null);
+      const result = await searchForShows(searchStr);
+      setApiData(result);
+    } catch (error) {
+      setApiErrorData(error);
+    }
   };
 
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div>Error Occured: {apiDataError.message}</div>;
+    }
+
+    if (apiData) {
+      return apiData.map(data => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+    return null;
+  };
   return (
     <div>
       <form onSubmit={onSearch}>
         <input type="text" value={searchStr} onChange={onSearchInputChange} />
         <button type="submit">Search</button>
       </form>
+
+      <div>{renderApiData()}</div>
     </div>
   );
 };
+
 export default Home;
