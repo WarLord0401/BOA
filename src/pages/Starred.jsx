c7cc9e4b950cdc55803b84470eeb5a3663c6019d
Kanishk <kanishkteotia5077@gmail.com>
2024-09-27 20:33:30 +0530

    displayed starred shows in starred page and added logic for error handling

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,9 +1,31 @@
+import { useQuery } from '@tanstack/react-query';
+import { getShowsByIds } from '../api/tvmaze';
+import ShowGrid from '../Components/shows/showGrid';
 import { useStarredShows } from '../lib/useStarredShows';
 
 const Starred = () => {
-  const [starredShows] = useStarredShows();
+  const [starredShowsIds] = useStarredShows();
 
-  return <div>Starred Shows... {starredShows.length}</div>;
+  const { data: starredShows, error: starredShowsError } = useQuery({
+    queryKey: ['starred', starredShowsIds],
+    queryFn: () =>
+      getShowsByIds(starredShowsIds).then(result =>
+        result.map(show => ({ show }))
+      ),
+    refetchOnWindowFocus: false,
+  });
+
+  if (starredShows?.length > 0) {
+    return <ShowGrid shows={starredShows} />;
+  }
+  if (starredShows?.length === 0) {
+    return <div>No Starred Shows Present</div>;
+  }
+  if (starredShowsError) {
+    return <div>Error Occurred: {starredShowsError.message}</div>;
+  }
+
+  return <div>Loading...</div>;
 };
 
 export default Starred;
